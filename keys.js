c21d5eb27c910c0c5b45f78515cefe01c64809f6
Ferenc Gutierrez <ferenc.g87@gmail.com>
2017-11-16 13:47:07 -0600

    Added all commands to LIRI and log functionality

--- keys.js
+++ keys.js
@@ -5,8 +5,17 @@ var twitterKeys = {
   access_token_secret: 'CX2Detm5Vn3jUHyTYorqAH7rVWIlHrfmDE2MO1OJkz6D1',
 }
 
+var spotifyKeys= {
+	id: "329b740270334bb79e12a4a12b71fb64",
+	secret: "cba99a21e15c4a52b871e6cf2bdaaf4e"
+}
+
+var omdbKey = {
+	api_key: "40e9cece"
+}
+
 module.exports = {
-	twitter:twitterKeys,
-	spotify: "None",
-	omdb: "Nothing"
+	twitter: twitterKeys,
+	spotify: spotifyKeys,
+	omdb: omdbKey
 }
