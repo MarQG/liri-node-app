de374ba580cba5f3d2df3bd8ed66cddd18bcc7f2
Ferenc Gutierrez <ferenc.g87@gmail.com>
2017-11-28 08:27:54 -0600

    Updated Liri to be more user friendly. Added Input validation and argument processing

--- liri.js
+++ liri.js
@@ -28,7 +28,7 @@ function getTweets() {
 
 function findMovie(movie) {
     var OMDBUrl = "http://www.omdbapi.com/?apikey=" + keys.omdb.api_key;
-    if (movie === undefined) {
+    if (movie === undefined || movie === "") {
         OMDBUrl += "&t=Mr+Nobody&type=movie";
     } else {
         OMDBUrl += "&t=" + movie + "&type=movie";
@@ -39,29 +39,35 @@ function findMovie(movie) {
         }
         if (response.statusCode === 200) {
             var foundMovie = JSON.parse(body);
-            appendLog("Title: " + foundMovie.Title);
-            appendLog("Year: " + foundMovie.Year);
-            appendLog("IMDB Rating: " + foundMovie.Ratings[0].Value);
-            if (foundMovie.Ratings.length > 1) {
-                appendLog("Rotten Tomatoes: " + foundMovie.Ratings[1].Value);
-            } else {
-                appendLog("Rotten Tomatoes: ");
-            }
-            appendLog("Country: " + foundMovie.Country);
-            appendLog("Plot: " + foundMovie.Plot);
-            appendLog("Actors: " + foundMovie.Actors);
+            if(foundMovie.Response === "True"){
+                appendLog("Title: " + foundMovie.Title);
+                appendLog("Year: " + foundMovie.Year);
+                appendLog("IMDB Rating: " + foundMovie.Ratings[0].Value);
+                if (foundMovie.Ratings.length > 1) {
+                    appendLog("Rotten Tomatoes: " + foundMovie.Ratings[1].Value);
+                } else {
+                    appendLog("Rotten Tomatoes: ");
+                }
+                appendLog("Country: " + foundMovie.Country);
+                appendLog("Plot: " + foundMovie.Plot);
+                appendLog("Actors: " + foundMovie.Actors);
 
-            console.log("Title: " + foundMovie.Title);
-            console.log("Year: " + foundMovie.Year);
-            console.log("IMDB Rating: " + foundMovie.Ratings[0].Value);
-            if (foundMovie.Ratings.length > 1) {
-                console.log("Rotten Tomatoes: " + foundMovie.Ratings[1].Value);
+                console.log("Title: " + foundMovie.Title);
+                console.log("Year: " + foundMovie.Year);
+                console.log("IMDB Rating: " + foundMovie.Ratings[0].Value);
+                if (foundMovie.Ratings.length > 1) {
+                    console.log("Rotten Tomatoes: " + foundMovie.Ratings[1].Value);
+                } else {
+                    console.log("Rotten Tomatoes: ");
+                }
+                console.log("Country: " + foundMovie.Country);
+                console.log("Plot: " + foundMovie.Plot);
+                console.log("Actors: " + foundMovie.Actors);
             } else {
-                console.log("Rotten Tomatoes: ");
+                appendLog("Sorry I couldn't find a movie named: " + movie);
+            console.log("Sorry I couldn't find a movie named: " + movie);
             }
-            console.log("Country: " + foundMovie.Country);
-            console.log("Plot: " + foundMovie.Plot);
-            console.log("Actors: " + foundMovie.Actors);
+            
         } else {
             console.log("Error code: " + response.statusCode);
         }
@@ -69,25 +75,37 @@ function findMovie(movie) {
 }
 
 function findSong(song) {
-    if (song === undefined) {
+    if (song === undefined || song === "") {
         song = "The Sign Ace of Base";
     }
     sptSearch.search({
         type: 'track',
         query: song,
-        limit: 1
+        limit: 5
     }, function (err, data) {
+
         if (err) {
             return console.log("Error occured: " + err);
         }
-        appendLog(data.tracks.items[0].album.artists[0].name);
-        appendLog(data.tracks.items[0].album.name);
-        appendLog(data.tracks.items[0].name);
-        appendLog(data.tracks.items[0].preview_url);
-        console.log(data.tracks.items[0].album.artists[0].name);
-        console.log(data.tracks.items[0].album.name);
-        console.log(data.tracks.items[0].name);
-        console.log(data.tracks.items[0].preview_url);
+        if(data.tracks.items.length > 0){
+            data.tracks.items.forEach(function(item){
+
+                appendLog("Song Name: " + item.name);
+                appendLog("Artist: " + item.album.artists[0].name);
+                appendLog("Album: " + item.album.name);
+                appendLog("Preview URL: " + item.preview_url);
+                appendLog("======================\n");
+
+                console.log("Song Name: " + item.name);
+                console.log("Artist: " + item.album.artists[0].name);
+                console.log("Album: " + item.album.name);
+                console.log("Preview URL: " + item.preview_url);
+                console.log("======================\n");
+            });
+        } else {
+            appendLog("Sorry I couldn't find a song named: " + song);
+            console.log("Sorry I couldn't find a song named: " + song);
+        }
     });
 }
 
@@ -103,7 +121,16 @@ function loadFile() {
     });
 }
 
-function liriWatcher(command, argument) {
+function processArg(arg){
+    var results = [];
+    arg.slice(3).forEach(function(item){
+        results.push(item);
+    });
+    return results.join(" ");
+}
+
+function liriWatcher(command) {
+    var argument = processArg(process.argv);
     appendLog(command + " " + argument);
     switch (command) {
         case "my-tweets":
@@ -119,8 +146,15 @@ function liriWatcher(command, argument) {
             loadFile();
             break;
         default:
-            console.log("Sorry I didn't understand that. Please try again.");
+            console.log(
+                "Sorry I didn't understand that.\n"+
+                "Please try one of the following commands: \n" +
+                "To get your tweets: my-tweets \n"+
+                "To get a song use: spotify-this-song <song_name> \n"+
+                "To get a movie use: movie-this <movie_name> \n"+
+                "I can read commands from files with: do-what-it-says \n"
+                );
     }
 }
 
-liriWatcher(process.argv[2], process.argv[3]);
+liriWatcher(process.argv[2]);
